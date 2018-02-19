869c8b2239544186ecf28d39ac2be6aa820adb96
ryan <ryanyt@hawaii.edu>
2018-02-18 19:03:40 -1000

    working, need to clean up

---
+++ view_vivaldi.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pprint
+
+# get each run of vivaldiUpdate
+raw_data = open("coordinate_table.txt", "r").read().strip('\n').split('*')
+
+# list of list(state at each call to vivaldi) of tuples(hostname, x, y)
+data = []
+
+# clean up 
+for block in raw_data:
+    if len(block) > 1:
+        coordinate_table_state = [] 
+    
+        block_contents = block.strip().split('\n')
+
+        for row in block_contents:
+            if len(row) > 1:
+                hostname, x, y = row.split('|')
+                row = (hostname.strip(), float(x), float(y))
+                coordinate_table_state.append(row)
+
+        data.append(coordinate_table_state)
+
+# reverse so i can pop off in the right order
+data.reverse()
+
+# plotting 
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+plt.subplots_adjust(bottom=0.1)
+
+colors = [i for i in range(len(data[0]))]
+
+def animate(i):
+    ax1.clear()
+    current_run = data.pop()
+
+    # output what is currently being plotted
+    pp = pprint.PrettyPrinter(indent=2) 
+    print("\ncoordinate table current state")
+    print("------------------------------")
+
+    pp.pprint(current_run)
+
+    hostnames, xvals, yvals = zip(*current_run)
+
+    # set up the labels
+    for host, x, y in zip(hostnames, xvals, yvals):
+        plt.annotate(host, xy=(x,y), xytext=(0,0),
+                textcoords='offset points', ha='right', va='bottom',
+                bbox=dict(boxstyle='round,pad=0.5', fc='grey', alpha=0.5))
+
+    plt.scatter(xvals, yvals, marker='o', c=colors, cmap=plt.get_cmap('Spectral'))
+
+
+# run the animation, refreshes at interval milliseconds, runs for frames, and stops on the last frame
+ani = animation.FuncAnimation(fig, animate, interval=1000, frames=len(data)-1, repeat=False)
+plt.show()
